a801ccb1e215c05cb9b6fef1cc6466a471c8a3df
Sergio Garrido <zx22student3212@colegiovalledelmiro.es>
2023-09-26 13:13:12 +0200

    CalculadoraV3

--- calculadora.py
+++ calculadora.py
@@ -3,6 +3,10 @@ y = int(input ("Introduce el segundo numero:"))
 
 suma = x + y
 resta = x - y
+multiplicacion = x * y
+
 print ("El resultado de la suma es: ", suma)
 print ("El resultado de la resta es: ", resta)
+print ("El resultado de la multiplicacion es: ", multiplicacion)
+
 
